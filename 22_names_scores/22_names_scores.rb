35d8ebcb2635f4d1ea94bd26647a6c38a85d8e2d
Karen Liu <karenliu1@gmail.com>
2014-09-29 08:18:08 -0700

    Use map.with_index in #22

--- 22_names_scores/22_names_scores.rb
+++ 22_names_scores/22_names_scores.rb
@@ -11,10 +11,9 @@ File.open('p022_names.txt', 'r') do |f|
   end
   names.sort!
 
-  total = 0
-  for i in 0..names.length - 1
-    total += (i+1) * name_value(names[i]) # i+1 because the 0th name is counted as the 1st
-  end
+  total = names
+    .map.with_index { |name, i| (i+1) * name_value(name) } # i+1 so 0th -> 1st
+    .reduce(:+)
 
   puts 'Total name score:', total
 end
